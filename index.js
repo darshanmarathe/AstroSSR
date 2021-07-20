9dc3bc694f7168536599fbb1348bcbe2f4dc5183
darshanmarathe <darshanmarathe@gmail.com>
2021-07-20 11:26:55 +0530

    added static for heroku

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static('dist'));
+
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist' ,'/index.html'));
